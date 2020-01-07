551e775ce60bc867a442b3909772546cbc7dfa4f
weisong <weisongyap@gmail.com>
2020-01-08 03:33:51 +0800

    34.2 Added Responsive Navigation And SideDrawer

--- src/components/Layout/Layouts.js
+++ src/components/Layout/Layouts.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from '../../hoc/Auxiliary'
 import classes from './Layout.css'
-const layout = (props) =>(
-  <Aux>
-    <div>   toolbar, sidedrawer, backdrop  </div>
-    <main className={classes.Content}>
-      {props.children}
-    </main>
-  </Aux>
-);
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar'
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
-export default layout;
+class Layout extends Component{
+    state = {
+        showSideDrawer:false
+    };
+
+    sideDrawerClosedHandler = () =>{
+        this.setState({showSideDrawer:false});
+    };
+    sideDrawerToggleHandler = () =>{
+        this.setState((prevState) =>{       //Correct way of referencing old state
+            return {showSideDrawer:!prevState.showSideDrawer };
+        });
+    };
+
+    render(){
+        return(
+          <Aux>
+              <div>   toolbar, sidedrawer, backdrop  </div>
+              <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
+              <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler}/>
+              <main className={classes.Content}>
+                  {this.props.children}
+              </main>
+          </Aux>
+        )
+    }
+}
+
+export default Layout;
